21540234a684637faff90159c11dff726a1d5a37
Vishal <vishalkmr363@gmail.com>
2018-04-02 18:27:41 +0530

    Miscleneous

---
+++ Miscellaneous/fibonacci.py
@@ -0,0 +1,14 @@
+''' 
+	Program to print fibonacci series
+	The Fibonacci numbers are the numbers in the following integer sequence.
+		0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+	In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation
+	    Fn = Fn-1 + Fn-2
+'''
+
+loop_counter=12
+a,b=0,1
+for i in range(loop_counter):
+	print(b)
+	a,b=b,a+b
